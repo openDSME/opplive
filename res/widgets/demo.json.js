8c2e55d0469a252117923df52a857916988a8db8
openDSME Demo <demo@opendsme.org>
2017-03-09 08:38:18 +0100

    Add demo layout

---
+++ res/widgets/demo.json.js
@@ -0,0 +1,81 @@
+var widget_positions_demo =
+    {
+        "reset_container": {
+            "x": 407,
+            "y": 810,
+            "w": null,
+            "h": null,
+            "hidden": false
+        },
+        "interval_container": {
+            "x": 50,
+            "y": 810,
+            "w": 250,
+            "h": null,
+            "hidden": false
+        },
+        "dsme_traffic_chart_container": {
+            "x": 1970,
+            "y": 160,
+            "w": 850,
+            "h": null,
+            "hidden": false
+        },
+        "csma_traffic_chart_container": {
+            "x": 2940,
+            "y": 160,
+            "w": 850,
+            "h": null,
+            "hidden": false
+        },
+        "positioning_container": {
+            "x": 50,
+            "y": 160,
+            "w": null,
+            "h": null,
+            "hidden": false
+        },
+        "widget_container": {
+            "x": 1170,
+            "y": 780,
+            "w": 340,
+            "h": null,
+            "hidden": true
+        },
+        "layout_container": {
+            "x": 640,
+            "y": 160,
+            "w": 250,
+            "h": null,
+            "hidden": false
+        },
+        "dsme_node_container": {
+            "x": 1970,
+            "y": 770,
+            "w": 850,
+            "h": null,
+            "hidden": false
+        },
+        "csma_node_container": {
+            "x": 2940,
+            "y": 770,
+            "w": 850,
+            "h": null,
+            "hidden": false
+        },
+        "power_chart_container": {
+            "x": 1020,
+            "y": 160,
+            "w": 850,
+            "h": null,
+            "hidden": false
+        },
+        "qr_code_container": {
+            "x": 1540,
+            "y": 710,
+            "w": null,
+            "h": null,
+            "hidden": false
+        }
+    }
+
