38a82e653ee1a5dfd39243c01c636c4b6a0d4bdb
openDSME Demo <demo@opendsme.org>
2017-03-10 15:00:10 +0100

    Finish animation loop

--- animation/animation.py
+++ animation/animation.py
@@ -4,22 +4,26 @@ from autopilot.input import Mouse
 import time
 
 mouse = Mouse.create()
-current_location = ( 960, 540)
+current_location = ( 960,  540)
 mouse.move(current_location[0], current_location[1], animate=False, rate=10, time_between_events=0.01)
 
 def move(location):
+    print("move " + str(location))
     global current_location
     mouse.move(location[0], location[1], animate=True, rate=10, time_between_events=0.01)
     current_location = location
 
 def click():
+    print("click")
     mouse.click()
 
 def drag(location):
+    print("drag " + str(location))
     global current_location
     mouse.drag(current_location[0], current_location[1], location[0], location[1], rate=10, time_between_events=0.01)
     current_location = location
 
 def sleep(s):
+    print("sleep " + str(s))
     time.sleep(s)
 
--- animation/demo.py
+++ animation/demo.py
@@ -7,14 +7,19 @@ loc_initial                 = ( 960,  540)
 
 loc_send_interval_initial   = ( 145,  865)
 loc_send_interval_3         = ( 100,  865)
+loc_send_interval_12        = ( 165,  865)
 
 loc_node_14_initial         = ( 105,  330)
 loc_node_14_final           = ( 545,  705)
+
+loc_concentric              = ( 725,  260)
 
 loc_reset                   = ( 490,  875)
 
 loc_dsme_traffic_top        = (2390,  185)
 loc_csma_traffic_top        = (3380,  185)
+
+loc_dsme_traffic_recent     = (2800,  495)
 
 while True:
     click()
@@ -26,17 +31,33 @@ while True:
     move(loc_send_interval_initial)
     sleep(1)
     drag(loc_send_interval_3)
-    sleep(15)
+    sleep(20)
+
+    drag(loc_send_interval_12)
+    sleep(20)
 
     move(loc_node_14_initial)
     sleep(1)
     drag(loc_node_14_final)
-    sleep(15)
+
+    sleep(5)
+    move(loc_dsme_traffic_recent)
+    sleep(30)
 
     move(loc_reset)
     sleep(1)
     click()
+    sleep(35)
+
+    move(loc_concentric)
     sleep(1)
+    click()
+    sleep(1)
+
+    move(loc_reset)
+    sleep(1)
+    click()
+    sleep(5)
 
     move(loc_initial)
     sleep(20)
